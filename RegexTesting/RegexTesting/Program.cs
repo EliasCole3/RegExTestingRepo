bae54b3307b0bf300d331444446d98aba2632257
EliasCole3 <eliascole3@gmail.com>
2014-05-16 15:33:29 -0700

    Fixed regex for Fabtrol to exclude comments

--- RegexTesting/RegexTesting/Program.cs
+++ RegexTesting/RegexTesting/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -69,8 +70,9 @@ namespace RegexTesting
             //string pattern = @"\b(test)\b";
             //string pattern = @"test";
             //string pattern = @"(?=.*\+=)(?!.*new)";//working
-            string pattern = @"(?=\+=)(?!new)";
+            //string pattern = @"(?=\+=)(?!new)";
             //string pattern = @"(?!new)(?=\+=)";
+            string pattern = @"^(?=.*\+=)(?!.*new)(?!.*//.*)";
 
 
             Regex rgx = new Regex(pattern, RegexOptions.IgnoreCase);
@@ -142,62 +144,54 @@ namespace RegexTesting
 
 
 
-//foreach (var filePath in filePathList)
-//{
-//    var lines = File.ReadAllLines(filePath)
-//        .Select(x => new {Line = x})
-//        .Where(x => !x.Line.Contains("this.Name = \""))/* only retrieving the line if a name declaration can be checked,
-//                                                            e.g. "this.Name = 'froglove'" can't be checked against itself.
-//                                                            "this.froglove.Name = 'froglove'" can be checked against itself. */
-//        .Where(x => !x.Line.Contains("//")) //We don't want any commented out code
-//        .Where(x => !x.Line.Contains("get {")) //We don't want properties
-//        .Where(x => !x.Line.Contains("set {")) //We don't want properties
-//        .Where(x => !x.Line.Contains("EventHandler")) //We don't want event handlers
-//        .Where(x => !x.Line.Contains(".Properties.Name"))
-//        .ToList();
 
-//    foreach (var anonLine in lines)
-//    {
-//        countOfFilteredLines++;
-//        string line = anonLine.Line; //Made a new anonymous type, getting the string property out of it
-//        line = line.Trim();
-//        var lineArray = line.Split(".".ToCharArray());
+namespace Fabtrol.McKenzie.Tests.MSTests.Localization
+{
 
-//        string toCompare1 = "";
-//        if (line.StartsWith("this.")) //some name assigments don't begin with "this.froglove.Name", just "froglove.Name"
-//        {
-//            toCompare1 = lineArray[1];
-//        }
-//        else
-//        {
-//            toCompare1 = lineArray[0];
-//        }
+    public class Test_EventHandlersUsingFullSyntax
+    {
 
-//        //getting the name of the right side of the assignment statement
-//        var toCompare2 = lineArray[lineArray.Length - 1];
-//        toCompare2 = toCompare2.TrimStart("Name = ".ToCharArray());
-//        //when on one line had an odd error, taking off the beginning m too
-//        toCompare2 = toCompare2.TrimStart("\"".ToCharArray());
-//        toCompare2 = toCompare2.TrimEnd("\";".ToCharArray());
+        public void Method_EventHandlersUsingFullSyntax()
+        {
+            string codePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            if (codePath.Contains("\\FabTrol"))
+            {
+                codePath = codePath.Substring(0, codePath.LastIndexOf("\\FabTrol"));
+            }
 
-//        if (toCompare1 != toCompare2)
-//        {
-//            if (toCompare2 == "")
-//            {
-//                countOfLinesWithEmptyNames++;
-//            }
-//            else
-//            {
-//                countOfDisparateNameLines++;
+            int countOfFilteredLines = 0;
 
-//                Console.WriteLine(filePath);
-//                Console.WriteLine(line);
-//                Console.WriteLine("First piece: " + toCompare1);
-//                //e.g. First piece is froglove in  "this.froglove.Name = 'froglove2'"
-//                Console.WriteLine("Second piece: " + toCompare2);
-//                //e.g. Second piece is froglove2 in  "this.froglove.Name = 'froglove2'"
-//                Console.WriteLine();
-//            }
-//        }
-//    }
-//}
+            var filePathList = Directory.GetFiles(codePath, "*designer.cs", SearchOption.AllDirectories).ToList(); //designer files
+
+            foreach (var filePath in filePathList)
+            {
+                var lines = File.ReadAllLines(filePath)
+                   .Select(x => new { Line = x })
+                   .Where(x => x.Line.Contains("+="))
+                   .Where(x => !x.Line.Contains("//"))
+                   .Where(x => !x.Line.Contains("new"))
+                   .ToList();
+
+                countOfFilteredLines += lines.Count;
+                if (lines.Count != 0)
+                {
+                    foreach (var line in lines)
+                    {
+                        Console.WriteLine(line);
+                        Console.WriteLine(filePath);
+                        Console.WriteLine();
+                    }
+                }
+            }
+
+            Console.WriteLine("number of designer files returned: " + filePathList.Count);
+            Console.WriteLine("Number of lines after being filtered by Ifs: " + countOfFilteredLines);
+
+            //Assert.IsTrue(countOfFilteredLines == 0);
+
+        }
+
+    }
+}
+
+
